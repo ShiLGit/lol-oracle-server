07bc722b9278ddd599f2063ed9db965d89cdb555
lilianshi00@gmail.com <lilianshi00@gmail.com>
2022-05-15 10:23:54 -0400

    init

---
+++ src/main/java/dilian/li/loloracleserver/LolOracleServerApplication.java
@@ -0,0 +1,13 @@
+package dilian.li.loloracleserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LolOracleServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LolOracleServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dilian/li/loloracleserver/LolOracleServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dilian.li.loloracleserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LolOracleServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
